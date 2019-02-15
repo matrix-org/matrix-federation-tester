4e53d549e3f78e18910ae3fabed5a54e2fe45b67
Brendan Abolivier <contact@brendanabolivier.com>
2019-02-15 19:27:07 +0000

    Make TravisCI use modules

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -2,6 +2,10 @@
 
 set -exu
 
+# Enable Go 1.11+ modules support in TravisCI.
+# See https://dave.cheney.net/2018/07/16/using-go-modules-with-travis-ci
+export GO111MODULE=on
+
 # run the tests
 go test
 
