e38f9c79ad56415b9b6738a534a553e4afa87fc8
Brendan Abolivier <contact@brendanabolivier.com>
2019-02-15 19:30:47 +0000

    Only enable modules when testing and building, not when linting

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -2,15 +2,11 @@
 
 set -exu
 
-# Enable Go 1.11+ modules support in TravisCI.
-# See https://dave.cheney.net/2018/07/16/using-go-modules-with-travis-ci
-export GO111MODULE=on
-
 # run the tests
-go test
+GO111MODULE=on go test
 
 # check it builds
-go build
+GO111MODULE=on go build
 
 # look for lint
 ./scripts/find-lint.sh
