6e5da0bca846e391d447f9f5a781c2020a93d3d1
Brendan Abolivier <contact@brendanabolivier.com>
2019-03-26 17:43:10 +0000

    Use go modules instead of gb (#46)

--- README.md
+++ README.md
@@ -6,18 +6,19 @@ Checks that federation is correctly configured on a matrix server.
 Building
 --------
 
-The tester is written in [golang](https://golang.org/) 1.10+ and built using [gb](https://getgb.io).
+The tester is written in [golang](https://golang.org/) 1.11+.
 
 ```bash
-go get github.com/constabulary/gb/...
-gb build
+git clone https://github.com/matrix-org/matrix-federation-tester
+cd matrix-federation-tester
+go build
 ```
 
 Running
 -------
 
 ```bash
-BIND_ADDRESS=:8080 bin/matrix-federation-tester
+BIND_ADDRESS=:8080 ./matrix-federation-tester
 ```
 
 Using
--- scripts/travis.sh
+++ scripts/travis.sh
@@ -2,11 +2,19 @@
 
 set -exu
 
+# We explicitely set the GO111MODULE in this script so that Go's module support
+# is enabled even though Travis CI does the checkout in the $GOPATH (in which
+# case the default behaviour is to disable module support). In a normal use,
+# setting this variable isn't necessary as the recommended install procedure is
+# via `git clone`, which makes it unlikely that the repository ever gets checked
+# out in the user's $GOPATH (in which case the default behaviour is to enable
+# module support, as long as the `go.mod` file exists).
+
 # run the tests
-gb test
+GO111MODULE=on go test
 
 # check it builds
-gb build
+GO111MODULE=on go build
 
 # look for lint
-./scripts/find-lint.sh
+GO111MODULE=off ./scripts/find-lint.sh
