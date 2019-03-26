bce3346313d63d069cd3dbd907a4adc3a27b802d
Brendan Abolivier <babolivier@matrix.org>
2019-03-26 16:59:01 +0000

    Add explanation on setting GO111MODULE in travis.sh

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -2,6 +2,14 @@
 
 set -exu
 
+# We explicitely set the GO111MODULE in this script so that Go's module support
+# is enabled even though Travis CI does the checkout in the $GOPATH (in which
+# case the default behaviour is to disable module support). In a normal use,
+# setting this variable isn't necessary as the recommended install procedure is
+# via `git clone`, which makes it unlikely that the repository ever gets checked
+# out in the user's $GOPATH (in which case the default behaviour is to enable
+# module support, as long as the `go.mod` file exists).
+
 # run the tests
 GO111MODULE=on go test
 
