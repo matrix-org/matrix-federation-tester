6368c0117f05a6bda23bbd59b6a17665f26fab31
Christopher May-Townsend <chris@maytownsend.co.uk>
2020-07-21 12:21:33 +0100

    Replace gometalinter with golangci (#107)
    Replace gometalinter with golangci
    
    * Fixes https://github.com/matrix-org/matrix-federation-tester/issues/74
     * using binary install of golang-ci
     * manually installing misspell now since seperate binary
     * removed missing linters
     * disabled gosec lint on debug endpoint
    
    * Also test for newer versions of Go
    
    Co-authored-by: Brendan Abolivier <babolivier@matrix.org>

--- scripts/find-lint.sh
+++ scripts/find-lint.sh
@@ -5,11 +5,11 @@ set -eu
 cd `dirname $0`/..
 
 echo "Installing lint search engine..."
-go get github.com/alecthomas/gometalinter/
-gometalinter --config=linter.json --install
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.27.0
 
 echo "Looking for lint..."
-gometalinter --config=linter.json
+$(go env GOPATH)/bin/golangci-lint run
 
 echo "Double checking spelling..."
-misspell -error src *.md
+go get -u github.com/client9/misspell/cmd/misspell
+$(go env GOPATH)/bin/misspell -error src *.md
